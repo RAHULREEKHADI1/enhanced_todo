40013e9a0d8d1ab7164b741d338ff49505a490c0
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2025-12-11 14:15:03 +0530

    cors updation

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://enhanced-todo-1.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
